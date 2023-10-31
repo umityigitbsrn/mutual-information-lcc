e8da1a64bfa28dd149eccfb6ca84ccc76ac6ba7d
umityigitbsrn <umityigitbsrn@gmail.com>
2023-10-31 15:45:41 -0700

    another implementation is tried for mutual information estimation, sss domain implementation is added

--- domain.py
+++ domain.py
@@ -1,6 +1,7 @@
 from polynomials import InterpolatedPoly
 import numpy as np
 import itertools
+from utils import pol_mul_mod
 
 
 def create_lcc_domain(K, data_range, T, prime, beta_arr):
@@ -27,3 +28,34 @@ def create_lcc_domain(K, data_range, T, prime, beta_arr):
         domain[idx, (3 * (K + T)):] = multiplied_poly.coefficients
 
     return domain
+
+
+def create_sss_dataset(prime, data_domain_range, num_of_samples, degree, norm=False):
+    # creating the dataset
+    ## starting from basic shamir secret sharing with real numbers
+    ## creating the polynomials first (reverse order - higher degree in the beginning)
+    first_poly_data = np.random.randint(data_domain_range, size=(num_of_samples, 1))
+    first_poly_coeff = np.random.randint(prime, size=(num_of_samples, degree))
+    first_poly_dataset = np.hstack([first_poly_data, first_poly_coeff])
+
+    # to create a resulting polynomial without any zero in the largest degree
+    second_poly_data = np.random.randint(data_domain_range, size=(num_of_samples, 1))
+    second_poly_coeff = np.random.randint(prime, size=(num_of_samples, degree))
+    second_poly_dataset = np.hstack([second_poly_data, second_poly_coeff])
+
+    multiplied_poly_dataset = np.empty((num_of_samples, 2 * degree + 1))
+    for idx in range(num_of_samples):
+        multiplied_poly_dataset[idx] = pol_mul_mod(first_poly_dataset[idx], second_poly_dataset[idx], prime)
+
+    mutual_information_dataset = np.empty((num_of_samples, 4 * degree + 3))
+    mutual_information_dataset[:, (2 * degree + 2):] = multiplied_poly_dataset
+    mutual_information_dataset[:, (degree + 1):(2 * degree + 2)] = second_poly_dataset
+    mutual_information_dataset[:, :(degree + 1)] = first_poly_dataset
+    mutual_information_dataset = mutual_information_dataset.astype(np.float64)
+
+    if norm:
+        mean_mi_dataset = np.mean(mutual_information_dataset)
+        std_mi_dataset = np.std(mutual_information_dataset)
+        mutual_information_dataset = (mutual_information_dataset - mean_mi_dataset) / std_mi_dataset
+
+    return mutual_information_dataset
